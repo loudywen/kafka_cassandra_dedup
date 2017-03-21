a893923c54abc839af41161b68f01229cfc6207f
Devon <loudywen@gmail.com>
2017-03-21 18:34:20 -0500

    added retry, but not working.
    TODO:
    1. fix retry
    2. handle exception

--- src/main/java/com/devon/demo/pojo_approch/KafkaPOJOConfig.java
+++ src/main/java/com/devon/demo/pojo_approch/KafkaPOJOConfig.java
@@ -10,10 +10,13 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.listener.AbstractMessageListenerContainer.AckMode;
+import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.adapter.FilteringAcknowledgingMessageListenerAdapter;
+import org.springframework.kafka.listener.adapter.RetryingAcknowledgingMessageListenerAdapter;
 import org.springframework.kafka.listener.config.ContainerProperties;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,7 +27,7 @@ import java.util.Map;
 public class KafkaPOJOConfig {
 
   public ConcurrentMessageListenerContainer<Integer, String> createContainer(
-      ContainerProperties containerProps, IKafkaConsumer iKafkaConsumer) {
+          ContainerProperties containerProps, IKafkaConsumer iKafkaConsumer) {
 
     Map<String, Object> props = consumerProps();
 
@@ -34,15 +37,37 @@ public class KafkaPOJOConfig {
     CustomKafkaMessageListener ckml = new CustomKafkaMessageListener(iKafkaConsumer);
     CustomRecordFilter cff = new CustomRecordFilter();
     FilteringAcknowledgingMessageListenerAdapter faml = new FilteringAcknowledgingMessageListenerAdapter(ckml,cff,true);
+    RetryTemplate rt = new RetryTemplate();
 
-    containerProps.setMessageListener(faml);
-    containerProps.setAckMode(AckMode.MANUAL_IMMEDIATE);
+    FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+    fixedBackOffPolicy.setBackOffPeriod(1000);
+    rt.setBackOffPolicy(fixedBackOffPolicy);
 
+    RetryingAcknowledgingMessageListenerAdapter rml = new RetryingAcknowledgingMessageListenerAdapter(faml,rt);
+
+    containerProps.setMessageListener(rml);
+    containerProps.setAckMode(AbstractMessageListenerContainer.AckMode.MANUAL_IMMEDIATE);
+    containerProps.setErrorHandler(ckml);
     ConcurrentMessageListenerContainer<Integer, String> container = new ConcurrentMessageListenerContainer<>(cf, containerProps);
-
     container.setConcurrency(10);
     return container;
   }
+
+
+/*public ConcurrentKafkaListenerContainerFactory<Integer, String> factory (ContainerProperties containerProps, IKafkaConsumer iKafkaConsumer){
+    ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory();
+    Map<String, Object> props = consumerProps();
+
+    DefaultKafkaConsumerFactory<Integer, String> cf = new DefaultKafkaConsumerFactory<Integer, String>(props);
+    concurrentKafkaListenerContainerFactory.setConsumerFactory(cf);
+    concurrentKafkaListenerContainerFactory.setConcurrency(10);
+    CustomKafkaMessageListener ckml = new CustomKafkaMessageListener(iKafkaConsumer);
+    concurrentKafkaListenerContainerFactory.getContainerProperties().setMessageListener(ckml);
+    concurrentKafkaListenerContainerFactory.setAutoStartup(true);
+
+
+    return concurrentKafkaListenerContainerFactory;
+}*/
 
   public KafkaTemplate<Integer, String> createTemplate() {
     Map<String, Object> senderProps = senderProps();
@@ -54,7 +79,9 @@ public class KafkaPOJOConfig {
 
   private Map<String, Object> consumerProps() {
     Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.28:9092");
+//    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.28:9092");
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.143.138:9092");
+
     props.put(ConsumerConfig.GROUP_ID_CONFIG, "pojokafka");
     props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
     //props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
@@ -66,7 +93,9 @@ public class KafkaPOJOConfig {
 
   private Map<String, Object> senderProps() {
     Map<String, Object> props = new HashMap<>();
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.28:9092");
+//    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.28:9092");
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.143.138:9092");
+
     props.put(ProducerConfig.RETRIES_CONFIG, 0);
     props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
     props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
