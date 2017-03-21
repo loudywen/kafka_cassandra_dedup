3536f0ab66f9dffc6911098832025703c993e258
Devon <loudywen@gmail.com>
2017-03-21 18:33:20 -0500

    added retry, but not working.
    TODO:
    1. fix retry
    2. handle exception

--- src/main/java/com/devon/demo/pojo_approch/CustomRecordFilter.java
+++ src/main/java/com/devon/demo/pojo_approch/CustomRecordFilter.java
@@ -3,12 +3,17 @@ package com.devon.demo.pojo_approch;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.listener.adapter.RecordFilterStrategy;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by diwenlao on 3/20/17.
  */
-public class CustomRecordFilter implements RecordFilterStrategy<Integer,String> {
+public class CustomRecordFilter implements RecordFilterStrategy<Integer, String> {
+    private AtomicLong retryCount = new AtomicLong();
+
     @Override
     public boolean filter(ConsumerRecord<Integer, String> consumerRecord) {
         return false;
+
     }
 }
